05f32442dcebe3072059ab1f8f30fe2cdfdc0926
mayanksaini2074-ui <mayank.saini2074@gmail.com>
2026-01-04 23:44:56 +0530

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,47 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation: {
+        shimmer: 'shimmer 2s linear infinite',
+        'spin-slow': 'spin 8s linear infinite',
+        'pulse-slow': 'pulse 3s ease-in-out infinite',
+        spotlight: 'spotlight 2s ease .75s 1 forwards',
+        aurora: 'aurora 60s linear infinite',
+      },
+      keyframes: {
+        shimmer: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        spotlight: {
+          '0%': {
+            opacity: '0',
+            transform: 'translate(-72%, -62%) scale(0.5)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translate(-50%,-40%) scale(1)',
+          },
+        },
+        aurora: {
+          from: {
+            backgroundPosition: '50% 50%, 50% 50%',
+          },
+          to: {
+            backgroundPosition: '350% 50%, 350% 50%',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
